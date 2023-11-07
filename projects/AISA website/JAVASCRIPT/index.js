8e9ba83b7f4e06b8502a31cd3e7288547992edf4
Sammed Koshti <sammedkoshti26@gmail.com>
2023-11-07 06:26:43 +0530

    notice board slider and below circle animation using javascript is done

--- projects/AISA website/JAVASCRIPT/index.js
+++ projects/AISA website/JAVASCRIPT/index.js
@@ -1,3 +1,6 @@
+
+// ==================== notice board ====================
+
 const all_notice = [document.querySelector(".notice1"), document.querySelector(".notice2"), document.querySelector(".notice3"), document.querySelector(".notice4"), document.querySelector(".notice5")];
 
 // for (let index = 0; index < all_notice.length; index++) {
@@ -26,6 +29,12 @@ const next_btn = document.querySelector(".next_btn");
 
 prev_btn.style.display = "none";
 
+// ------------ notice board below circles -----------
+
+const circles = [document.querySelector(".notice-board-circles .button1"), document.querySelector(".notice-board-circles .button2"), document.querySelector(".notice-board-circles .button3"), document.querySelector(".notice-board-circles .button4"), document.querySelector(".notice-board-circles .button5")];
+console.log(circles);
+
+circles[0].style.background = "rgba(0, 0, 0, 0.7)";
 
 prev_btn.addEventListener("click", ()=>{
     count--;
@@ -39,7 +48,12 @@ prev_btn.addEventListener("click", ()=>{
     }
     all_notice[count+1].style.display = "none";
     all_notice[count].style.display = "block";
+
+    circles[count+1].style.background = "rgba(0, 0, 0, 0.2)";
+    circles[count].style.background = "rgba(0, 0, 0, 0.7)";
 })
+
+
 
 
 next_btn.addEventListener("click", ()=>{
@@ -54,6 +68,14 @@ next_btn.addEventListener("click", ()=>{
     }
     all_notice[count-1].style.display = "none";
     all_notice[count].style.display = "block";
+
+    circles[count-1].style.background = "rgba(0, 0, 0, 0.2)";
+    circles[count].style.background = "rgba(0, 0, 0, 0.7)";
 })
+
+
+
+
+
 
 
