93c0744dd27e1ef354a93318671f7f3fa3584c19
Sammed Koshti <sammedkoshti26@gmail.com>
2023-11-18 09:04:08 +0530

    AISA website home page is done and mini project's machine learning model is implemented

---
+++ python codes/ml lab work/Exp 2/data_preprocessing.py
@@ -0,0 +1,9 @@
+# data preprocessing 
+
+# importing libraries
+import pandas as pd
+import numpy as np
+
+# importing dataset
+dataset = pd.read_csv("./preprocessing_car_dataset.csv")
+print(dataset);
---
+++ python codes/ml lab work/data_preprocessing.py
@@ -0,0 +1,8 @@
+# data preprocessing 
+
+# importing libraries
+import numpy as np;
+import pandas as pd;
+
+
+# data set importing
